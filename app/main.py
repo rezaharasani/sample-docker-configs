ce8def5132c769c5544d9b4226181940ab6babec
rezaharasani <rharasani208@gmail.com>
2025-07-15 07:47:12 +0330

    Change the long input parameter into ** formant (e.g. **post.dict())

--- app/main.py
+++ app/main.py
@@ -61,8 +61,7 @@ def get_posts(db: Session = Depends(get_db)):
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post, db: Session = Depends(get_db)):
     """Create a new post"""
-    new_post = models.Post(
-        title=post.title, content=post.content, published=post.published)
+    new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
