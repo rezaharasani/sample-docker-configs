be1a51ec0f49567f067c930b7b1e07614658de5d
rezaharasani <rharasani208@gmail.com>
2025-07-15 17:26:23 +0330

    add some extra features and parameters to FastAPI class, like following part: ``` app = FastAPI(     title="PANDA",     description="Python and FastAPI Project",     version="v0.1.0",     contact={"responsible_email": "panda@gmail.com"},     docs_url="/docs",     redoc_url="/redoc",     deprecated=False ) ```

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
-from typing import List
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import user, post
-from fastapi import FastAPI, status, Response, HTTPException, Depends
-from . import models, schemas, utils
-from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="PANDA",
+    description="Python and FastAPI Project",
+    version="v0.1.0",
+    contact={"responsible_email": "panda@gmail.com"},
+    docs_url="/docs",
+    redoc_url="/redoc",
+    deprecated=False
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
 
 
-@app.get("/", status_code=status.HTTP_200_OK)
+@app.get("/")
 def root():
-    """display root page"""
     return {"message": "welcome to my api"}
