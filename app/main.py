d3adf2efeeb0d79fc1766e3ef4494e58796c7c79
rezaharasani <rharasani208@gmail.com>
2025-07-15 09:49:51 +0330

    add response model to all possible functions

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, status, Response, HTTPException, Depends
 from sqlalchemy.orm import Session
 from . import models, schemas
@@ -14,34 +15,45 @@ def root():
     return {"message": "welcome to my api"}
 
 
-@app.get("/posts", status_code=status.HTTP_200_OK)
+@app.get("/posts",
+         status_code=status.HTTP_200_OK,
+         response_model=List[schemas.Post]
+)
 def get_posts(db: Session = Depends(get_db)):
     """Get all posts"""
-    posts = db.query(models.Post).all()
-    return {"response": posts}
+    posts = db.query(models.Post).order_by(models.Post.id).all()
+    return posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
+@app.post("/posts",
+          status_code=status.HTTP_201_CREATED,
+          response_model=schemas.Post
+)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     """Create a new post"""
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"response": new_post}
+    return new_post
 
 
-@app.get("/posts/latest", status_code=status.HTTP_200_OK)
+@app.get("/posts/latest",
+         status_code=status.HTTP_200_OK,
+         response_model=schemas.Post
+)
 def get_latest_post(db: Session = Depends(get_db)):
     """Get latest post"""
     lastest_post = db.query(models.Post).order_by(models.Post.created_at.desc()).first()
 
     if lastest_post is None:
         return {"response": f"There is no post yet."}
-    return {"response": lastest_post}
+    return lastest_post
 
 
-@app.get("/posts/{post_id}")
+@app.get("/posts/{post_id}",
+         response_model=schemas.Post
+)
 def get_post(post_id: int, db: Session = Depends(get_db)):
     """Get post by id"""
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
@@ -49,10 +61,12 @@ def get_post(post_id: int, db: Session = Depends(get_db)):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {post_id} does not found.")
-    return {"response": post}
+    return post
 
 
-@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/posts/{post_id}",
+            status_code=status.HTTP_204_NO_CONTENT
+)
 def delete_post(post_id: int, db: Session = Depends(get_db)):
     """Delete post by id"""
     post_query = db.query(models.Post).filter(models.Post.id == post_id)
@@ -66,7 +80,10 @@ def delete_post(post_id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{post_id}", status_code=status.HTTP_201_CREATED)
+@app.put("/posts/{post_id}",
+         status_code=status.HTTP_201_CREATED,
+         response_model=schemas.Post
+)
 def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     """Update post"""
     post_query = db.query(models.Post).filter(models.Post.id == post_id)
@@ -78,4 +95,4 @@ def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(ge
     db.query(models.Post).filter(models.Post.id == post_id).update(post.model_dump(),
                                                                    synchronize_session=False)
     db.commit()
-    return {"response": post_query.first()}
+    return post_query.first()
