7e7de89b31eceeb6ec66791367fb77e9d00d533b
rezaharasani <rharasani208@gmail.com>
2025-07-15 09:11:37 +0330

    add some small changes in format

--- app/main.py
+++ app/main.py
@@ -45,6 +45,7 @@ def get_latest_post(db: Session = Depends(get_db)):
 def get_post(post_id: int, db: Session = Depends(get_db)):
     """Get post by id"""
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {post_id} does not found.")
@@ -69,10 +70,12 @@ def delete_post(post_id: int, db: Session = Depends(get_db)):
 def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     """Update post"""
     post_query = db.query(models.Post).filter(models.Post.id == post_id)
+
     if post_query.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {post_id} does not found.")
 
-    db.query(models.Post).filter(models.Post.id == post_id).update(post.model_dump(), synchronize_session=False)
+    db.query(models.Post).filter(models.Post.id == post_id).update(post.model_dump(),
+                                                                   synchronize_session=False)
     db.commit()
     return {"response": post_query.first()}
