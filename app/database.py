e0a03bc5f5e0f3322cf6d0ccc6be1e57ef159bea
rezaharasani <rharasani208@gmail.com>
2025-07-17 17:53:13 +0330

    Using environment variables

--- app/database.py
+++ app/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password123@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = \
+    (f"postgresql://"
+     f"{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}"
+     f"@{settings.DATABASE_HOSTNAME}/{settings.DATABASE_NAME}")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
