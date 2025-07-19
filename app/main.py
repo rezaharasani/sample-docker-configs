80bb28b9dc2f2a87e7f20364504b5a62fa59c4e5
rezaharasani <rharasani208@gmail.com>
2025-07-19 08:40:58 +0330

    add vote router to the main part

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import user, post, auth
+from .routers import user, post, auth, vote
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,7 +9,6 @@ app = FastAPI(
     title="PANDA",
     description="Python and FastAPI Project",
     version="v0.1.0",
-    contact={"responsible_email": "panda@gmail.com"},
     docs_url="/docs",
     redoc_url="/redoc",
     deprecated=False
@@ -17,6 +16,7 @@ app = FastAPI(
 
 app.include_router(user.router)
 app.include_router(post.router)
+app.include_router(vote.router)
 app.include_router(auth.router)
 
 
